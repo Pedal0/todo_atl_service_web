da7c0c2d93a791f6d1cdd3bf4e513b616a60c304
Pedal0 <perax33@gmail.com>
2025-05-14 12:47:48 +0200

    feat: update title in Home component to reflect development status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-5xl font-bold">Ma TODO List</h1>
+      <h1 className="text-5xl font-bold">Ma TODO List (Developpement)</h1>
       <Link href="/contact" className="mt-4">
         Contact
       </Link>
